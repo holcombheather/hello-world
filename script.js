7e98206d9d9f7d1a1b0648e363ace997783c46e8
Heather Holcomb <holcombheather@gmail.com>
2023-02-23 14:26:47 -0800

    Updated HTML and added CSS

---
+++ script.js
@@ -0,0 +1,65 @@
+let myName = "Heather"; 
+alert("Welcome to my website!" + myName);
+
+// 4 ways to declare a variable (create one)
+
+// let <-- changeable variable 
+// const <-- constant variable, can't be change
+// var <-- bad practice in 102
+// (none) <--bad practice in 102
+
+// DATA TYPES
+// string --> "text", "42", ""
+// numbers --> 42, 123, 456, 
+// boolean --> true/false 
+
+//console.log("Hello world!");
+
+//let myFavColor = "yellow";
+//console.log(myFavColor);
+
+// myFavColor = "purple";
+//console.log(myFavColor);
+
+//let userName= prompt("Enter your name");
+//console.log(userName); 
+
+//alert("Hello," + userName)
+
+// WRITING TO THE HTML DOCUMENT
+
+//document.write("Hello world");
+//document.write("Welcome," + userName);
+
+//let coffeeChoice = prompt("Do you like coffee? Type yes or no");
+//console.log(coffeeChoice);
+
+// structure of conditionals
+// if(this is true){exectute code} 
+
+// if(coffeeChoice == 'yes'){
+//    document.write("I like coffee too."); 
+// } else if (coffeeChoice == 'no')
+
+// Anatomy of a function
+
+// function declaration / function definition
+// function functionName(parameters){code to execute goes here;}
+
+
+
+
+
+
+
+
+
+// Refactoring code in functions
+
+//  let myName = "Heather"; 
+//  alert("Welcome to my website!" + myName);
+
+function greeting(){
+    let userName = prompt("Enter your user name");
+
+}
