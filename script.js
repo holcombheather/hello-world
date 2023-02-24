2a832cf950c1786f3a56ebcdfc2e7d496174f6ab
Heather Holcomb <holcombheather@gmail.com>
2023-02-23 23:39:21 -0800

    Updated website to meet requirements for Lab07

--- script.js
+++ script.js
@@ -27,35 +27,72 @@
 // }
 
 
+
+
+
 //Lab07: Refactoring code with functions
 
 
 function greeting(){
     let userName = prompt("Welcome to 102 coffee! What is your name?");
     console.log(userName);
-    // prompt("Hi " + userName + "! Do you love coffee?")
     document.write("Hello, " + userName + "! ");
-    document.write("Welcome to your new favorite place.");
-    return userName;
-}
 
-function lovesCoffee(){
-    let coffeeChoice = prompt("Do you love coffee? Type yes or no");
-    // let coffeeChoice = prompt("Hi " + userName + ". Do you love coffee? Type yes or no");
+    let coffeeChoice = prompt("Hi " + userName + "! Do you love coffee? Type yes or no");
     console.log(coffeeChoice);
-    
     if(coffeeChoice == 'yes'){
         document.write("You love coffee.");
         let howMany = prompt("How many cups do you drink a day?");
+        console.log(howMany); 
+            if(howMany > '1'){
+               let promoCode = prompt("We agree. One is never enough. Enter in your email and we’ll send you a promo code for " + howMany + " free coffees on us!");
+                console.log(promoCode)
+                document.write(" We sent you " + howMany + " free coffees to your inbox at " + promoCode);
+            if(howMany <= '1'){
+                prompt("With just one you've got to make it count. Opt-in to our marketing emails and we'll send you a promo code for a free coffee on us.")
+            }
+            }
     } else if (coffeeChoice == 'no'){
-        document.write("You don't love coffee but you'll love us.");
-        // let bevChoice = prompt("What is your favorite beverage?")
+        document.write("You don't love coffee but you'll love us. Opt-in to our emails and get a free beverage of your choice on us!");
     } else {
-        document.write("We're not sure if you like coffee but we know you'll love us.")
-    }
-    }
+        document.write("We're not sure if you like coffee but we know you'll like us!")
+}   
+        function getUserInput() {
+            let maxAttempts = 3; // Maximum number of attempts
+            let attempts = 1; // Counter for the number of attempts
+          
+            function isValidInput(input) {
+              // Check if the input is either 'yes' or 'no'
+              return input.toLowerCase() === 'yes' || input.toLowerCase() === 'no';
+            }
+          
+            let userInput = prompt("Please enter 'yes' or 'no': ");
+          
+            while (!isValidInput(userInput)) {
+              if (attempts >= maxAttempts) {
+                alert(`You have exceeded the maximum number of attempts (${maxAttempts}).`);
+                return null;
+              }
+              alert("Invalid input. Please try again.");
+              userInput = prompt(`Please enter 'yes' or 'no' (attempt ${attempts}): `);
+              attempts++;
+            }
+          
+            return userInput.toLowerCase();
+          }
+          
+          // Call the getUserInput function to get user input
+          let userInput = getUserInput();
+          
+          // Do something with the valid user input
+          if (userInput !== null) {
+            alert(`You entered: ${userInput}`);
+          }
+
+
+
+    } 
     
 
-greeting();
 
-lovesCoffee();
+greeting();
