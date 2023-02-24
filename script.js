67fdeaf82c306de493384a676475360608e45fc8
Heather Holcomb <holcombheather@gmail.com>
2023-02-24 15:05:50 -0800

    Fixed CSS for Section 5

--- script.js
+++ script.js
@@ -33,27 +33,27 @@
 // Lab07: Refactoring code with functions
 
 
-function greeting(){
+function greeting() {
     let userName = prompt("Welcome to 102 coffee! What is your name?");
     console.log(userName);
     document.write("Hello, " + userName + "! ");
-   
+
 
     let coffeeChoice = prompt("Hi " + userName + "! Do you love coffee? Type yes or no");
     console.log(coffeeChoice);
-    if(coffeeChoice == 'yes'){
+    if (coffeeChoice == 'yes') {
         document.write("You love coffee.");
         let howMany = prompt("How many cups do you drink a day?");
-        console.log(howMany); 
-            if(howMany > '1'){
-               let promoCode = prompt("We agree. One is never enough. Enter in your email and we’ll send you a promo code for " + howMany + " free coffees on us!");
-                console.log(promoCode)
-                document.write(" We sent you " + howMany + " free coffees to your inbox at " + promoCode);
-            if(howMany <= '1'){
+        console.log(howMany);
+        if (howMany > '1') {
+            let promoCode = prompt("We agree. One is never enough. Enter in your email and we’ll send you a promo code for " + howMany + " free coffees on us!");
+            console.log(promoCode)
+            document.write(" We sent you " + howMany + " free coffees to your inbox at " + promoCode);
+            if (howMany <= '1') {
                 prompt("With just one you've got to make it count. Opt-in to our marketing emails and we'll send you a promo code for a free coffee on us.")
             }
-            }
-    } else if (coffeeChoice == 'no'){
+        }
+    } else if (coffeeChoice == 'no') {
         document.write("You don't love coffee but you'll love us. Opt-in to our emails and get a free beverage of your choice on us!");
     } else {
         document.write("We're not sure if you like coffee but we know you'll like us!")
@@ -61,48 +61,48 @@ function greeting(){
         function getUserInput() {
             let maxAttempts = 3; // Maximum number of attempts
             let attempts = 1; // Counter for the number of attempts
-          
+
             function isValidInput(input) {
-              // Check if the input is either 'yes' or 'no'
-              return input.toLowerCase() === 'yes' || input.toLowerCase() === 'no';
+                // Check if the input is either 'yes' or 'no'
+                return input.toLowerCase() === 'yes' || input.toLowerCase() === 'no';
             }
             let userInput = prompt("Please enter 'yes' or 'no': ");
             while (!isValidInput(userInput)) {
-              if (attempts >= maxAttempts) {
-                alert(`You have exceeded the maximum number of attempts (${maxAttempts}).`);
-                return null;
-              }
-              alert("Invalid input. Please try again.");
-              userInput = prompt(`Please enter 'yes' or 'no' (attempt ${attempts}): `);
-              attempts++;
+                if (attempts >= maxAttempts) {
+                    alert(`You have exceeded the maximum number of attempts (${maxAttempts}).`);
+                    return null;
+                }
+                alert("Invalid input. Please try again.");
+                userInput = prompt(`Please enter 'yes' or 'no' (attempt ${attempts}): `);
+                attempts++;
             }
             return userInput.toLowerCase();
-          }
-          // Call the getUserInput function to get user input
-          let userInput = getUserInput(); 
-          // Do something with the valid user input
-          if (userInput !== null) {
-            alert(`You entered: ${userInput}`);
-          }
         }
-    } 
-    
+        // Call the getUserInput function to get user input
+        let userInput = getUserInput();
+        // Do something with the valid user input
+        if (userInput !== null) {
+            alert(`You entered: ${userInput}`);
+        }
+    }
+}
 
-    greeting();
+
+greeting();
 
 
-function coffeeCups(){
+function coffeeCups() {
     let coffeePic = parseInt(prompt("How many cups of coffee have you had today? Pick a number between 0 and 10"));
-    console.log ("user had " + coffeePic + "cups of coffee");
-    if (coffeePic <= 10){
+    console.log("user had " + coffeePic + "cups of coffee");
+    if (coffeePic <= 10) {
         alert("Nice!");
         console.log("user had" + coffeePic + "cups of coffee");
-    for (let i = 0; i < coffeePic; i++){
+        for (let i = 0; i < coffeePic; i++) {
             document.write('<img src="images/coffee_logo_background.png" height="20px" width="20px">');
         }
-        
+
         return coffeeCups;
-    } else if (coffeePic > 10){
+    } else if (coffeePic > 10) {
         alert("Whoa! That's a lot! For the sake of this conversation, let's just pick a number between 0 and 10.");
         console.log("user had" + coffeePic + "cups of coffee");
         coffeeCups()
